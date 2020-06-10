d33939ec08d32b0f7b2bd05a94dcb78b6bdd40a8
Melody-Ruth <melody@sd-fun.org>
2020-06-10 08:15:13 -0700

    testing buttons

---
+++ main.js
@@ -0,0 +1,11 @@
+function onHomeHover() {
+	var home = document.getElementById("HomeButton");
+	home.style.backgroundColor = "#97ccf7";
+	home.style.color = "#005fad";
+}
+
+function onHomeNotHover() {
+	var home = document.getElementById("HomeButton");
+	home.style.backgroundColor = "#c7e6ff";
+	home.style.color = "#0275d4";
+}
