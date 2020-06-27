8bec03e34c75c9d2275f1acc1d505db4a53de88c
Melody-Ruth <melody@sd-fun.org>
2020-06-27 11:53:15 -0700

    working on contact me

--- contactMe/code/contactMe.js
+++ contactMe/code/contactMe.js
@@ -1,31 +1,7 @@
-function onHomeHover() {
-	var home = document.getElementById("HomeButton");
-	home.style.backgroundColor = "#97ccf7";
-	home.style.color = "#005fad";
-}
-
-function onHomeNotHover() {
-	var home = document.getElementById("HomeButton");
-	home.style.backgroundColor = "#c7e6ff";
-	home.style.color = "#0275d4";
-}
-
-function homeClicked() {
-	document.location = '../../index.htm';
-}
-
-function onContactHover() {
-	var contact = document.getElementById("ContactButton");
-	contact.style.backgroundColor = "#b1f0b2";
-	contact.style.color = "#54ab54";
-}
-
-function onContactNotHover() {
-	var contact = document.getElementById("ContactButton");
-	contact.style.backgroundColor = "#d4ffd4";
-	contact.style.color = "#67c267";
-}
-
-function contactClicked() {
-	document.location = 'contactMe.htm';
+function loadSecret() {
+	var myEmail = document.getElementById("email");
+	var firstPart = "test";
+	var secondPart = "ing";
+	var thirdPart = "testing.com";
+	//myEmail.innerHTML = firstPart + secondPart + "@" + thirdPart;
 }
